d78ac23cf2f4279afa2eeca74f1483d8aaf216ff
raluiz <45321496+raluiz@users.noreply.github.com>
2019-06-15 22:03:50 -0300

    Simple Sum

--- 1003.c
+++ 1003.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 int main() {
-	//Create two integers.
+	//Create two integers
 	int a, b;
 	
-	//Read a value for each variable.
+	//Read a value for each variable
 	scanf("%d %d", &a, &b);
 	
-	//Print SOMA = a + b.
+	//Print SOMA = a + b
 	printf("SOMA = %d\n", a + b);
 	
 	return 0;
