ae04b5bf815a77615134df460ed2b6668b67dda3
raluiz <45321496+raluiz@users.noreply.github.com>
2019-06-15 21:38:23 -0300

    Problem 1002

--- 1002.c
+++ 1002.c
@@ -2,10 +2,14 @@
 #include <stdlib.h>
 
 int main() {
+	//Create a double variable.
 	double r;
 	
+	//Read a value.
 	scanf("%lf", &r);
 	
+	//Print the area of a circle using pi = 3.14159.
 	printf("A=%.4lf\n", (3.14159 * r * r)); 
+	
 	return 0;
 }
