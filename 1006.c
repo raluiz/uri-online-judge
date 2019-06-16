0bb18db7e9edd425c48ceffb8f7d50a7b5bb0575
raluiz <45321496+raluiz@users.noreply.github.com>
2019-06-15 22:05:47 -0300

    Average 2

--- 1006.c
+++ 1006.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 int main() {
-    //Create three variables.
+	//Create three variables.
 	double a, b, c;
 	
-    //Read a value for each variable.
+	//Read a value for each variable.
 	scanf("%lf %lf %lf", &a, &b, &c);
 	
-    //Print MEDIA = (a * 2 + b * 3 + c * 5) / 10.
-    //2, 3 and 5 are weights students grades.
+	//Print MEDIA = (a * 2 + b * 3 + c * 5) / 10.
+	//2, 3 and 5 are weights students grades.
 	printf("MEDIA = %.1lf\n", (a*2 + b*3 + c*5)/10);
 	return 0;
 }
