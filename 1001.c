3d66a1a9b7af5ccb73833235ecef642c7e083fca
raluiz <45321496+raluiz@users.noreply.github.com>
2019-06-15 21:34:49 -0300

    Problem 1001

--- 1001.c
+++ 1001.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 
 int main() {
+	//Create two integers.
 	int a, b;
 	
+	//Read two integers.
 	scanf("%d %d", &a, &b);
 	
+	//Print X = a + b.
 	printf("X = %d\n", a + b);
 	
 	return 0;
