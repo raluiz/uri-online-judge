835180c7e8ed457573ff3e97f154891bd9d48e17
raluiz <45321496+raluiz@users.noreply.github.com>
2019-06-15 21:49:18 -0300

    Problem 1004

---
+++ 1004.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  //Create two integers.
+	int a, b;
+  
+  //Read a value for each variable.
+	scanf("%d %d", &a, &b);
+  
+  //Print PROD = a * b
+  printf("PROD = %d\n", a * b);
+  
+	return 0;
+}
