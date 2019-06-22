a6cbdb8f9cbd378b56111f7416fd8779d0af07d9
raluiz <45321496+raluiz@users.noreply.github.com>
2019-06-22 17:44:54 -0300

    Simple Product

--- 1004.c
+++ 1004.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 int main() {
-	//Create two integers
+	//Create two integers.
 	int a, b;
   
-	//Read a value for each variable
+	//Read a value for each variable.
 	scanf("%d %d", &a, &b);
   
-	//Print PROD = a * b
+	//Print PROD = <a * b>.
 	printf("PROD = %d\n", a * b);
 	
 	return 0;
