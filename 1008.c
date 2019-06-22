85ad21ef3202417813998177ed178012cc110e62
raluiz <45321496+raluiz@users.noreply.github.com>
2019-06-22 16:15:56 -0300

    Salary

---
+++ 1008.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    //Create two integers and one double variable.
+	int n, wh;
+	double aphw;
+	
+    //Read a value for each variable.
+	scanf("%d\n%d\n%lf", &n, &wh, &aphw);
+	
+    //Print the employee's number.
+	printf("NUMBER = %d\n", n);
+    
+    //Print the employee's salary (wh * aphw).
+	printf("SALARY = U$ %.2lf\n", wh * aphw);
+    
+	return 0;
+}
