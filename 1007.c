c97be7309ccec144fdc7735e18bc536a7b4709a6
raluiz <45321496+raluiz@users.noreply.github.com>
2019-06-22 16:20:05 -0300

    Difference

--- 1007.c
+++ 1007.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 int main() {
-    //Create four integers.
+        //Create four integers.
 	int a, b, c, d;
 	
-    //Read a value for each integer.
+        //Read a value for each integer.
 	scanf("%d\n%d\n%d\n%d\n", &a, &b, &c, &d);
 	
-    //Print the result of a * b - c * d.
+        //Print the result of a * b - c * d.
 	printf("DIFERENCA = %d\n", a*b - c*d);
     
 	return 0;
