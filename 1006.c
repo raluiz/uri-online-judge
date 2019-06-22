b22e44e44229dbba515a03fa3e47d9d55d85ee3e
raluiz <45321496+raluiz@users.noreply.github.com>
2019-06-22 17:46:01 -0300

    Average 2

--- 1006.c
+++ 1006.c
@@ -8,7 +8,7 @@ int main() {
 	//Read a value for each variable.
 	scanf("%lf %lf %lf", &a, &b, &c);
 	
-	//Print MEDIA = (a * 2 + b * 3 + c * 5) / 10.
+	//Print MEDIA = <(a * 2 + b * 3 + c * 5) / 10>.
 	//2, 3 and 5 are weights students grades.
 	printf("MEDIA = %.1lf\n", (a*2 + b*3 + c*5)/10);
 	return 0;
