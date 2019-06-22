5e27738100af8a70e978489e61b2b6732cb5ba72
raluiz <45321496+raluiz@users.noreply.github.com>
2019-06-22 17:45:35 -0300

    Average 1

--- 1005.c
+++ 1005.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 int main() {
-	//Create two variables
+	//Create two variables.
 	double a, b;
 	
-	//Read a value for each variable
+	//Read a value for each variable.
 	scanf("%lf %lf", &a, &b);
 	
-	//Print MEDIA = (a * 3.5 + b * 7.5) / (3.5 + 7.5)
-	//3.5 and 7.5 are weights for the students grades
+	//Print MEDIA = <(a * 3.5 + b * 7.5) / (3.5 + 7.5)>.
+	//3.5 and 7.5 are weights for the students grades.
 	printf("MEDIA = %.5lf\n", (a * 3.5 + b * 7.5)/(3.5 + 7.5));
   
 	return 0;
