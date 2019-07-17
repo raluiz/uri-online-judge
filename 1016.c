a3f61c3cc6be21b113c0d5c6f29b0b99097d02e5
raluiz <45321496+raluiz@users.noreply.github.com>
2019-07-17 17:03:57 -0300

    Distance

---
+++ 1016.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	//Create an integer.
+	int d;
+	
+	//Read a value.
+	scanf("%d", &d);
+	
+	//Print <d * 2> minutos.
+	printf("%d minutos\n", d * 2);
+	
+	return 0;
+}
